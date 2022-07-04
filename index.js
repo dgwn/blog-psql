856996829b5b01c28010eb01c230ecbc20a97330
dgwn <danielgawne@gmail.com>
2022-07-03 22:26:36 -0400

    transform into web app which supports GET, POST, DELETE operations

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 const { Sequelize, Model, DataTypes, QueryTypes } = require("sequelize");
+const express = require("express");
+const app = express();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialectOptions: {
@@ -43,14 +45,48 @@ Blog.init(
   }
 );
 
+app.get("/api/blogs", async (req, res) => {
+  const blogs = await Blog.findAll();
+  res.json(blogs);
+});
+
+app.use(express.json());
+
+app.post("/api/blogs", async (req, res) => {
+  try {
+    const blog = await Blog.create(req.body);
+    return res.json(blog);
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
+});
+
+app.delete("/api/blogs/:id", async (req, res) => {
+  try {
+    const blog = await Blog.findOne({where: {id: Number(req.params.id)}})
+    if (blog) {
+      await blog.destroy({where: {id: Number(req.params.id)}})
+      return (res.status(200).json({ message: "Resource Deleted" }))
+    }
+    return(res.status(404).json({ message: "Resource does not exist"}))
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
+})
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
 const main = async () => {
   try {
     await sequelize.authenticate()
 
-    const blogs = await sequelize.query("SELECT * FROM blogs", {type: QueryTypes.SELECT})
-    console.log(blogs)
+    // const blogs = await sequelize.query("SELECT * FROM blogs", {type: QueryTypes.SELECT})
+    // console.log(blogs)
 
-    sequelize.close()
+    // sequelize.close()
   } catch (error) {
     console.error('Unable to connect to the database:', error)
   }
