06d76834b0fa9ea6544e3aea1f5db72aa4e78692
dgwn <danielgawne@gmail.com>
2022-07-08 17:53:53 -0400

    exercise 13.5 - restructure app

---
+++ controllers/blogs.js
@@ -0,0 +1,33 @@
+const router = require('express').Router()
+
+const { Blog } = require('../models')
+
+router.get("/", async (req, res) => {
+  const blogs = await Blog.findAll();
+  res.json(blogs);
+});
+
+
+router.post("/", async (req, res) => {
+  try {
+    const blog = await Blog.create(req.body);
+    return res.json(blog);
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const blog = await Blog.findOne({where: {id: Number(req.params.id)}})
+    if (blog) {
+      await blog.destroy({where: {id: Number(req.params.id)}})
+      return (res.status(200).json({ message: "Resource Deleted" }))
+    }
+    return(res.status(404).json({ message: "Resource does not exist"}))
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
+})
+
+module.exports = router
