cb4c6af2c1e463ae32e92db8a03565a34609c792
dgwn <danielgawne@gmail.com>
2022-07-10 20:28:02 -0400

    exercise 13.7 - move POST and PUT error handling into custom middleware

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,18 +2,25 @@ const router = require('express').Router()
 
 const { Blog } = require('../models')
 
+const errorHandler = (error, req, res, next) => {
+  console.log("1");
+  console.error(`Error(s): [${error.message}]`);
+  res.status(400).json({ error });
+  next()
+}
+
 router.get("/", async (req, res) => {
   const blogs = await Blog.findAll();
   res.json(blogs);
 });
 
 
-router.post("/", async (req, res) => {
+router.post("/", async (req, res, next) => {
   try {
     const blog = await Blog.create(req.body);
     return res.json(blog);
   } catch (error) {
-    return res.status(400).json({ error });
+    next(error)
   }
 });
 
@@ -30,17 +37,25 @@ router.delete("/:id", async (req, res) => {
   }
 })
 
-router.put("/:id", async (req, res) => {
+router.put("/:id", async (req, res, next) => {
   try {
     const blog = await Blog.findOne({where: {id: Number(req.params.id)}})
     if (blog) {
-      await blog.update({likes: req.body.likes})
-      return res.json(blog);
+      if (req.body.likes) {
+        await blog.update({likes: req.body.likes})
+        return res.json(blog);
+      }
+      
+      const e =  new Error("'likes' parameter is invalid")
+      e.name = "invalid parameters"
+      throw e
     }
     return(res.status(404).json({ message: "Resource does not exist"}))
   } catch (error) {
-    return res.status(400).json({ error });
+    next(error)
   }
 })
+
+router.use(errorHandler)
 
 module.exports = router
