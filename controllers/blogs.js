b0a817566afb01134b7d7895789e7635909965c2
dgwn <danielgawne@gmail.com>
2022-07-10 23:18:48 -0400

    add express-async-errors package and reformat with prettier

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,61 +1,51 @@
-const router = require('express').Router()
+const router = require("express").Router();
 
-const { Blog } = require('../models')
+const { Blog } = require("../models");
 
 const errorHandler = (error, req, res, next) => {
-  console.log("1");
   console.error(`Error(s): [${error.message}]`);
   res.status(400).json({ error });
-  next()
-}
+  next();
+};
 
 router.get("/", async (req, res) => {
   const blogs = await Blog.findAll();
   res.json(blogs);
 });
 
-
-router.post("/", async (req, res, next) => {
-  try {
-    const blog = await Blog.create(req.body);
-    return res.json(blog);
-  } catch (error) {
-    next(error)
-  }
+router.post("/", async (req, res) => {
+  const blog = await Blog.create(req.body);
+  return res.json(blog);
 });
 
 router.delete("/:id", async (req, res) => {
   try {
-    const blog = await Blog.findOne({where: {id: Number(req.params.id)}})
+    const blog = await Blog.findOne({ where: { id: Number(req.params.id) } });
     if (blog) {
-      await blog.destroy()
-      return (res.status(200).json({ message: "Resource Deleted" }))
+      await blog.destroy();
+      return res.status(200).json({ message: "Resource Deleted" });
     }
-    return(res.status(404).json({ message: "Resource does not exist"}))
+    return res.status(404).json({ message: "Resource does not exist" });
   } catch (error) {
     return res.status(400).json({ error });
   }
-})
+});
 
-router.put("/:id", async (req, res, next) => {
-  try {
-    const blog = await Blog.findOne({where: {id: Number(req.params.id)}})
-    if (blog) {
-      if (req.body.likes) {
-        await blog.update({likes: req.body.likes})
-        return res.json(blog);
-      }
-      
-      const e =  new Error("'likes' parameter is invalid")
-      e.name = "invalid parameters"
-      throw e
+router.put("/:id", async (req, res) => {
+  const blog = await Blog.findOne({ where: { id: Number(req.params.id) } });
+  if (blog) {
+    if (req.body.likes) {
+      await blog.update({ likes: req.body.likes });
+      return res.json(blog);
     }
-    return(res.status(404).json({ message: "Resource does not exist"}))
-  } catch (error) {
-    next(error)
+
+    const e = new Error("'likes' parameter is invalid");
+    e.name = "invalid parameters";
+    throw e;
   }
-})
+  return res.status(404).json({ message: "Resource does not exist" });
+});
 
-router.use(errorHandler)
+router.use(errorHandler);
 
-module.exports = router
+module.exports = router;
--- index.js
+++ index.js
@@ -1,109 +1,21 @@
 // const { Sequelize, Model, DataTypes, QueryTypes } = require("sequelize");
 const express = require("express");
 const app = express();
+require("express-async-errors");
 
-const { PORT } = require('./util/config')
-const { connectToDatabase } = require('./util/db')
+const { PORT } = require("./util/config");
+const { connectToDatabase } = require("./util/db");
 
-const blogsRouter = require('./controllers/blogs')
+const blogsRouter = require("./controllers/blogs");
 
-app.use(express.json())
-app.use('/api/blogs', blogsRouter)
+app.use(express.json());
+app.use("/api/blogs", blogsRouter);
 
 const start = async () => {
-  await connectToDatabase()
-  app.listen(PORT, () => {console.log(`Server running on port ${PORT}`)})
-}
+  await connectToDatabase();
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+};
 
-start()
-
-// const sequelize = new Sequelize(process.env.DATABASE_URL, {
-//   dialectOptions: {
-//     ssl: {
-//       require: true,
-//       rejectUnauthorized: false,
-//     },
-//   },
-// });
-
-// class Blog extends Model {}
-// Blog.init(
-//   {
-//     id: {
-//       type: DataTypes.INTEGER,
-//       primaryKey: true,
-//       autoIncrement: true,
-//     },
-//     author: {
-//       type: DataTypes.TEXT,
-//       allowNull: true,
-//     },
-//     url: {
-//       type: DataTypes.TEXT,
-//       allowNull: false,
-//     },
-//     title: {
-//       type: DataTypes.TEXT,
-//       allowNull: false,
-//     },
-//     likes: {
-//       type: DataTypes.INTEGER,
-//       defaultValue: 0
-//     }
-//   },
-//   {
-//     sequelize,
-//     underscored: true,
-//     timestamps: false,
-//     modelName: "blog",
-//   }
-// );
-
-// app.get("/api/blogs", async (req, res) => {
-//   const blogs = await Blog.findAll();
-//   res.json(blogs);
-// });
-
-// app.use(express.json());
-
-// app.post("/api/blogs", async (req, res) => {
-//   try {
-//     const blog = await Blog.create(req.body);
-//     return res.json(blog);
-//   } catch (error) {
-//     return res.status(400).json({ error });
-//   }
-// });
-
-// app.delete("/api/blogs/:id", async (req, res) => {
-//   try {
-//     const blog = await Blog.findOne({where: {id: Number(req.params.id)}})
-//     if (blog) {
-//       await blog.destroy({where: {id: Number(req.params.id)}})
-//       return (res.status(200).json({ message: "Resource Deleted" }))
-//     }
-//     return(res.status(404).json({ message: "Resource does not exist"}))
-//   } catch (error) {
-//     return res.status(400).json({ error });
-//   }
-// })
-
-// const PORT = process.env.PORT || 3001;
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
-
-// const main = async () => {
-//   try {
-//     await sequelize.authenticate()
-
-//     // const blogs = await sequelize.query("SELECT * FROM blogs", {type: QueryTypes.SELECT})
-//     // console.log(blogs)
-
-//     // sequelize.close()
-//   } catch (error) {
-//     console.error('Unable to connect to the database:', error)
-//   }
-// }
-
-// main()
+start();
