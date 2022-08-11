4a95f59da5da71e7ac3866bca75e4206c010caac
dgwn <danielgawne@gmail.com>
2022-08-11 16:33:57 -0400

    exercise 13.13 - implement title filtering with query paramaters

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 
+const { Op } = require("sequelize");
 const jwt = require("jsonwebtoken");
 const { SECRET } = require("../util/config");
 
@@ -26,12 +27,20 @@ const tokenExtractor = (req, res, next) => {
 };
 
 router.get("/", async (req, res) => {
+  const where = {};
+
+  if (req.query.search) {
+    where.title = {
+      [Op.substring]: req.query.search
+    };
+  }
   const blogs = await Blog.findAll({
     attributes: { exclude: ["userId"] },
     include: {
       model: User,
       attributes: ["name"]
-    }
+    },
+    where
   });
   res.json(blogs);
 });
