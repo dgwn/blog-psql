0fe9bdea3e22ebec95303e11b9174293f6ae0de0
dgwn <danielgawne@gmail.com>
2022-08-12 20:11:47 -0400

    exercise 13.16 - make a rout that returns number of blogs and total number of likes for each author, then sort by total number of likes

---
+++ controllers/authors.js
@@ -0,0 +1,19 @@
+const router = require("express").Router();
+
+const { Blog, User } = require("../models");
+const { sequelize } = require("../models/blog");
+
+router.get("/", async (req, res) => {
+  const authors = await Blog.findAll({
+    group: "author",
+    order: [[sequelize.fn("SUM", sequelize.col("likes")), "DESC"]],
+    attributes: [
+      "author",
+      [sequelize.fn("COUNT", sequelize.col("title")), "articles"],
+      [sequelize.fn("SUM", sequelize.col("likes")), "total_likes"]
+    ]
+  });
+  res.json(authors);
+});
+
+module.exports = router;
