6a19687a16bf2c29c004674efb643489c02635fa
dgwn <danielgawne@gmail.com>
2022-09-25 14:36:22 -0400

    exercise 13.19 - give users ability to add blogs to reading list in db

--- models/index.js
+++ models/index.js
@@ -1,10 +1,14 @@
 const Blog = require("./blog");
 const User = require("./user");
+const ReadingList = require("./readingList");
 
 User.hasMany(Blog);
 Blog.belongsTo(User);
 
+Blog.belongsToMany(User, { through: ReadingList });
+
 module.exports = {
   Blog,
-  User
+  User,
+  ReadingList
 };
